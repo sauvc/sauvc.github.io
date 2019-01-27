e470f7c57f4dcb9680ae2256bb13861c29736c6f
Chinmay Pendharkar <notthetup@gmail.com>
2019-01-27 12:03:07 +0800

    fixing eslint errors

--- data/addflags.js
+++ data/addflags.js
@@ -5,12 +5,12 @@ const teams = require('./registered.json');
 
 teams.forEach((team) => {
 	let found = flags.find((flag) => {
-		return team.country === flag.name
+		return team.country === flag.name;
 	});
 
 	if (found) team.flag = found.emoji;
 	// console.log(team.country,team.flag);
-})
+});
 
 fs.writeFile('registered-teams.json',  JSON.stringify(teams), 'utf8', function(){});
 
